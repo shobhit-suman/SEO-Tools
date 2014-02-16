bafb9842273679f3ca7bfb6fa6f87487d2e3f32e
xpd54 <xpd54@localhost.localdomain>
2014-02-16 23:15:52 +0530

    php files

---
+++ automation#8/auto_8.js
@@ -0,0 +1,71 @@
+// note: save the script (above) as a file, and include() it at the bottom of all pages
+
+var qs=after("?",var HTTP_SERVER_VARS['HTTP_REFERER']); 
+var dn=before("?",HTTP_SERVER_VARS['HTTP_REFERER']); 
+var pairs=explode("&",qs); 
+if (eregi("google",dn)){ 
+for (var k in pairs) {
+       var v = pairs[k]; 
+var args=explode("=",v); 
+if (args[0]=="q" ¦¦ args[0]=="as_q" ¦¦ args[0]=="as_epq"){ 
+if (strlen(args)>0 && args[1]!="0"){ 
+var keys=urldecode(args[1]); 
+} 
+} 
+} 
+}else if(eregi("msn",dn)){ 
+for (var k in pairs) {
+       var v = pairs[k]; 
+args=explode("=",v); 
+if (args[0]=="q"){ 
+if (strlen(args[1])>0 && args[1]!="0"){ 
+keys=urldecode(args[1]); 
+} 
+} 
+} 
+}else if(eregi("yahoo",dn)){ 
+for (var k in pairs) {
+       var v = pairs[k]; 
+args=explode("=",v); 
+if (args[0]=="p"){ 
+if (strlen(args[1])>0 && args[1]!="0"){ 
+keys=urldecode(args[1]); 
+} 
+} 
+} 
+}else if(eregi("aol",dn)){ 
+for (var k in pairs) {
+       var v = pairs[k]; 
+args=explode("=",v); 
+if (args[0]=="query"){ 
+if (strlen(args[1])>0 && args[1]!="0"){ 
+keys=urldecode(args[1]); 
+} 
+} 
+} 
+}else if(eregi("hotbot",dn)){ 
+for (var k in pairs) {
+       var v = pairs[k]; 
+args=explode("=",v); 
+if (args[0]=="query"){ 
+if (strlen(args[1])>0 && args[1]!="0"){ 
+keys=urldecode(args[1]); 
+} 
+} 
+} 
+}
+
+if ((keys) && strlen(keys)>0){ 
+mysql_select_db("mydbase"); 
+var query="INSERT INTO searchterms (referertoURL,keywords,refererURL,datetime,userIP) VALUES ('http://" + "" + HTTP_SERVER_VARS['SERVER_NAME'] + "" + HTTP_SERVER_VARS['SCRIPT_NAME']  + "" + "','" + "" + strtolower(trim(keys)) + "" + "','" + "" + HTTP_SERVER_VARS['HTTP_REFERER']  + "" + "','"  + "" + date("Y-m-d h:i:s",mktime()) + "" + "','" + "" + HTTP_SERVER_VARS['REMOTE_ADDR'] + "" + "')"; 
+mysql_query(query); 
+}
+
+function after  (this, inthat){ 
+if (is_bool(strpos(inthat, this))) 
+return substr(inthat, strpos(inthat,this)+strlen(this)); 
+}
+
+function before  (this, inthat){ 
+return substr(inthat, 0, strpos(inthat, this)); 
+}
---
+++ automation#8/search_keyword_php.php
@@ -0,0 +1,70 @@
+<?php 
+
+// note: save the script (above) as a file, and include() it at the bottom of all pages
+
+$qs=after("?",$HTTP_SERVER_VARS['HTTP_REFERER']); 
+$dn=before("?",$HTTP_SERVER_VARS['HTTP_REFERER']); 
+$pairs=explode("&",$qs); 
+if (eregi("google",$dn)){ 
+foreach ($pairs as $k=>$v){ 
+$args=explode("=",$v); 
+if ($args[0]=="q" ¦¦ $args[0]=="as_q" ¦¦ $args[0]=="as_epq"){ 
+if (strlen($args)>0 && $args[1]!="0"){ 
+$keys=urldecode($args[1]); 
+} 
+} 
+} 
+}elseif(eregi("msn",$dn)){ 
+foreach ($pairs as $k=>$v){ 
+$args=explode("=",$v); 
+if ($args[0]=="q"){ 
+if (strlen($args[1])>0 && $args[1]!="0"){ 
+$keys=urldecode($args[1]); 
+} 
+} 
+} 
+}elseif(eregi("yahoo",$dn)){ 
+foreach ($pairs as $k=>$v){ 
+$args=explode("=",$v); 
+if ($args[0]=="p"){ 
+if (strlen($args[1])>0 && $args[1]!="0"){ 
+$keys=urldecode($args[1]); 
+} 
+} 
+} 
+}elseif(eregi("aol",$dn)){ 
+foreach ($pairs as $k=>$v){ 
+$args=explode("=",$v); 
+if ($args[0]=="query"){ 
+if (strlen($args[1])>0 && $args[1]!="0"){ 
+$keys=urldecode($args[1]); 
+} 
+} 
+} 
+}elseif(eregi("hotbot",$dn)){ 
+foreach ($pairs as $k=>$v){ 
+$args=explode("=",$v); 
+if ($args[0]=="query"){ 
+if (strlen($args[1])>0 && $args[1]!="0"){ 
+$keys=urldecode($args[1]); 
+} 
+} 
+} 
+}
+
+if (isSet($keys) && strlen($keys)>0){ 
+mysql_select_db("mydbase"); 
+$query="INSERT INTO searchterms (referertoURL,keywords,refererURL,datetime,userIP) VALUES ('http://".$HTTP_SERVER_VARS['SERVER_NAME'].$HTTP_SERVER_VARS['SCRIPT_NAME'] ."','".strtolower(trim($keys))."','".$HTTP_SERVER_VARS['HTTP_REFERER'] ."','" .date("Y-m-d h:i:s",mktime())."','".$HTTP_SERVER_VARS['REMOTE_ADDR']."')"; 
+mysql_query($query); 
+}
+
+function after ($this, $inthat){ 
+if (!is_bool(strpos($inthat, $this))) 
+return substr($inthat, strpos($inthat,$this)+strlen($this)); 
+}
+
+function before ($this, $inthat){ 
+return substr($inthat, 0, strpos($inthat, $this)); 
+}
+
+?>
